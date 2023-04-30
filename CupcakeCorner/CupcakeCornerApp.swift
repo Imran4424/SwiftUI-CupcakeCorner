988624e0543796ad38b72d53702974931a5deafa
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2023-05-01 03:09:45 +0600

    project iit

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Shah Md Imran Hossain on 1/5/23.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
