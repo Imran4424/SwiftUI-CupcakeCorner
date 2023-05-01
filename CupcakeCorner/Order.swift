9e0c40adbabddd5223c1004c6f058c7961f55b91
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2023-05-01 14:33:04 +0600

    Order added

---
+++ CupcakeCorner/Order.swift
@@ -0,0 +1,19 @@
+//
+//  Order.swift
+//  CupcakeCorner
+//
+//  Created by Shah Md Imran Hossain on 1/5/23.
+//
+
+import SwiftUI
+
+class Order: ObservableObject {
+    static let types = ["Vanilla", "Strawberry", "Chocolate", "Rainbow"]
+    
+    @Published var type = 0
+    @Published var quantity = 3
+    
+    @Published var specialRequestEnabled = false
+    @Published var extraFrosting = false
+    @Published var addSprinkles = false
+}
