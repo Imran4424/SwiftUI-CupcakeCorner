cf8b48189d8bc5a8af2d0eb28c152f3a28fc186a
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2023-05-01 14:41:10 +0600

    fix the bug with didst

--- CupcakeCorner/Order.swift
+++ CupcakeCorner/Order.swift
@@ -13,7 +13,17 @@ class Order: ObservableObject {
     @Published var type = 0
     @Published var quantity = 3
     
-    @Published var specialRequestEnabled = false
+    @Published var specialRequestEnabled = false {
+        // didSet is called after value is set
+        // after value is set
+        // do post processing what you need to be done
+        didSet {
+            if specialRequestEnabled == false {
+                extraFrosting = false
+                addSprinkles = false
+            }
+        }
+    }
     @Published var extraFrosting = false
     @Published var addSprinkles = false
 }
